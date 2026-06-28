483860f60948fdc7c502a633ef83cfcedd580165
Laserology <admin@laserology.net>
2026-06-28 09:23:29 -0700

    Unify config

--- LSTWC.php
+++ LSTWC.php
@@ -22,54 +22,11 @@ class LSTWC {
 
     /**
      * Init and hook in the integration.
+     * This boots the report submodule.
      */
     public function __construct() {
-        add_filter('woocommerce_adjust_non_base_location_prices', '__return_false');
-
-        // Boot the report submodule.
         require_once plugin_dir_path( __FILE__ ) . 'src/Init.php';
         \TweaksForWoo\Init::boot();
-
-        add_action('woocommerce_new_order', [$this, 'LSTWC_force_billing_address']);
-    }
-
-    /**
-     * Fill in billing address from store base if it's missing.
-     *
-     * This ensures the Woo mobile app always has a billing address on
-     * in-person orders, which is required for compliance and records.
-     *
-     * Only applies to orders created by users with 'manage_options' capability.
-     */
-    public static function LSTWC_force_billing_address( $order_id ) {
-        // Only proceed if we can positively identify the user as having admin.
-        if ( ! current_user_can( 'manage_options' ) ) {
-            return;
-        }
-
-        // Get order object from ID.
-        $order = wc_get_order( $order_id );
-
-        // Avoid setting orders that have information set already.
-        if ( $order && ! $order->has_shipping_address() ) {
-            $countries = WC()->countries;
-
-            // Set shipping address.
-            $order->set_shipping_country( $countries->get_base_country() );
-            $order->set_shipping_state( $countries->get_base_state() );
-            $order->set_shipping_city( $countries->get_base_city() );
-            $order->set_shipping_address_1( $countries->get_base_address() );
-            $order->set_shipping_postcode( $countries->get_base_postcode() );
-
-            // Set billing address.
-            $order->set_billing_country( $countries->get_base_country() );
-            $order->set_billing_state( $countries->get_base_state() );
-            $order->set_billing_city( $countries->get_base_city() );
-            $order->set_billing_address_1( $countries->get_base_address() );
-            $order->set_billing_postcode( $countries->get_base_postcode() );
-
-            $order->save();
-        }
     }
 }
 
--- src/Init.php
+++ src/Init.php
@@ -17,6 +17,14 @@ class Init {
 	public static function boot(): void {
 		self::register_autoloader();
 
+		if ( \TweaksForWoo\Report\ConfigView::is_location_adjust_enabled() ) {
+		    add_filter('woocommerce_adjust_non_base_location_prices', '__return_false');
+		}
+
+		if ( \TweaksForWoo\Report\ConfigView::is_billing_tweak_enabled() ) {
+		    add_action('woocommerce_new_order', [__CLASS__, 'LSTWC_force_billing_address']);
+		}
+
 		// Hook into WooCommerce menu so our tab appears alongside the default reports.
 		// Only run if user has selected for it to be enabled.
         if ( \TweaksForWoo\Report\ConfigView::is_ca_tax_screen_enabled() ) {
@@ -29,6 +37,45 @@ class Init {
 		// Enqueue report styles on the WooCommerce admin pages.
 		add_action( 'admin_enqueue_scripts', [ __CLASS__, 'enqueue_assets' ] );
 	}
+
+	/**
+     * Fill in billing address from store base if it's missing.
+     *
+     * This ensures the Woo mobile app always has a billing address on
+     * in-person orders, which is required for compliance and records.
+     *
+     * Only applies to orders created by users with 'manage_options' capability.
+     */
+    public static function LSTWC_force_billing_address( $order_id ) {
+        // Only proceed if we can positively identify the user as having admin.
+        if ( ! current_user_can( 'manage_options' ) ) {
+            return;
+        }
+
+        // Get order object from ID.
+        $order = wc_get_order( $order_id );
+
+        // Avoid setting orders that have information set already.
+        if ( $order && ! $order->has_shipping_address() ) {
+            $countries = WC()->countries;
+
+            // Set shipping address.
+            $order->set_shipping_country( $countries->get_base_country() );
+            $order->set_shipping_state( $countries->get_base_state() );
+            $order->set_shipping_city( $countries->get_base_city() );
+            $order->set_shipping_address_1( $countries->get_base_address() );
+            $order->set_shipping_postcode( $countries->get_base_postcode() );
+
+            // Set billing address.
+            $order->set_billing_country( $countries->get_base_country() );
+            $order->set_billing_state( $countries->get_base_state() );
+            $order->set_billing_city( $countries->get_base_city() );
+            $order->set_billing_address_1( $countries->get_base_address() );
+            $order->set_billing_postcode( $countries->get_base_postcode() );
+
+            $order->save();
+        }
+    }
 
 	/**
 	 * Simple class autoloader for src/ classes.
